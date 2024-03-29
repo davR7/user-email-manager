bf702348be66531dcf674f2d52d7b467752b14c3
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-29 13:41:53 -0300

    created(ms-email) - EmailConsumer class

---
+++ ms-email/src/main/java/com/davr7/msemail/consumers/EmailConsumer.java
@@ -0,0 +1,23 @@
+package com.davr7.msemail.consumers;
+
+import com.davr7.msemail.dtos.EmailDTO;
+import com.davr7.msemail.services.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @RabbitListener(queues = "${broker.queue.name}" )
+    public void listenEmailQueue(@Payload EmailDTO emailDTO) {
+        System.out.println(emailDTO);
+        emailService.sendEmail(emailDTO);
+    }
+}
