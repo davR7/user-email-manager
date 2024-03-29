706c5050ea574f2359ddb7ce670845e854ec16ec
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-29 11:45:33 -0300

    created(ms-user) - types

---
+++ ms-user/src/common/types.ts
@@ -0,0 +1,42 @@
+import { HttpError } from "./HttpError";
+import { Request, Response, NextFunction } from "express";
+import { ParamsDictionary as Params, Query, Send } from "express-serve-static-core";
+
+export interface IRequestBody<T> extends Request {
+  body: T;
+}
+
+export interface IRequestParams<T extends Params> extends Request {
+  params: T;
+}
+
+export interface IRequestQuery<T extends Query> extends Request {
+  query: T;
+}
+
+export interface IResquest<ReqBody = any, ReqParams extends Params = any, ReqQuery extends Query = any>
+  extends Request {
+  body: ReqBody;
+  params: ReqParams;
+  query: ReqQuery;
+}
+
+export interface IResponse<ResBody = any> extends Response {
+  json: Send<ResBody, this>;
+}
+
+export interface INextFunction extends NextFunction {}
+
+export interface IExpressContext<
+  ReqBody = any,
+  ResBody = any,
+  ReqParams extends Params = any,
+  ReqQuery extends Query = any,
+> {
+  apiRequest(): IResquest<ReqBody, ReqParams, ReqQuery>;
+  apiReqBody(): ReqBody;
+  apiReqParams(): ReqParams;
+  apiResponse(): IResponse<ResBody>;
+  apiNext(value?: HttpError): HttpError | void;
+  apiSend(body?: ResBody, status?: number): IResponse<ResBody>;
+}
