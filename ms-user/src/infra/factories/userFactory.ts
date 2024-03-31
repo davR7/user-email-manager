a784510de8c550c77ba6c72cf2c2537afd2c8219
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-31 11:18:23 -0300

    updated(ms-user) - added instance Bcript class to the UserService class

--- ms-user/src/infra/factories/userFactory.ts
+++ ms-user/src/infra/factories/userFactory.ts
@@ -3,11 +3,13 @@ import { PrismaUserRepo } from "../repositories/PrismaUserRepo";
 import { UserService } from "../../services/UserService";
 import { UserResource } from "../../resources/UserResource";
 import { UserProducer } from "../../producers/UserProducer";
+import { Bcrypt } from "../password-hashing/Bcrypt";
 
 export const userFactory = () => {
   const prismaUserRepo = new PrismaUserRepo();
   const userProducer = new UserProducer(rabbitMQ);
-  const userService = new UserService(prismaUserRepo, userProducer);
+  const bcrypt = new Bcrypt();
+  const userService = new UserService(prismaUserRepo, userProducer, bcrypt);
   const userResource = new UserResource(userService);
   return userResource;
 };
