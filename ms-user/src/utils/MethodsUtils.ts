e1247825020210e7a6020e272de2e16bb2e49b9d
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-31 10:30:36 -0300

    updated(ms-user) - renamed targetClass for TargetClass of MethodsUtils class

--- ms-user/src/utils/MethodsUtils.ts
+++ ms-user/src/utils/MethodsUtils.ts
@@ -1,10 +1,8 @@
 class MethodsUtils {
   copyProperties(source: any, target: any): void {
-    if (!(source instanceof Object))
-      throw new Error("source must be an object");
+    if (!(source instanceof Object)) throw new Error("source must be an object");
 
-    if (!(target instanceof Object))
-      throw new Error("target must be an object");
+    if (!(target instanceof Object)) throw new Error("target must be an object");
 
     for (let prop in target) {
       if (source.hasOwnProperty(prop)) {
@@ -13,8 +11,8 @@ class MethodsUtils {
     }
     return target;
   }
-  arrayDto(arr: any[], targetClass: new (...args: any[]) => any) {
-    return arr.map((obj) => this.copyProperties(obj, new targetClass()));
+  arrayDto(arr: any[], TargetClass: new (...args: any[]) => any) {
+    return arr.map(obj => this.copyProperties(obj, new TargetClass()));
   }
 }
 
