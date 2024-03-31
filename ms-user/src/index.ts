70e88c2c759eeb9ca83ed3b0da16bfdd3e0c4ba4
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-31 13:58:01 -0300

    updated(ms-user) - instanced App Class with arguments app and express

--- ms-user/src/index.ts
+++ ms-user/src/index.ts
@@ -1,7 +1,9 @@
-import { App } from "./app";
+import { App } from "./infra/web-server/app";
+import express from "express";
 
+const app = express();
 const port = process.env.APP_PORT;
 
-new App().listenPort(port, () => {
+new App(app, express).listenPort(port, () => {
   console.log("Server listening on: " + port);
 });
