67b5972ed7a44f58df488d374662977bee7d9ea6
Davidson Ratis <davidsonmakarios@gmail.com>
2024-04-01 17:48:26 -0300

    updated(ms-email) - removed handleSendEmail method and modified endpoint get for email of the EmailResource class

--- ms-email/src/main/java/com/davr7/msemail/resources/EmailResource.java
+++ ms-email/src/main/java/com/davr7/msemail/resources/EmailResource.java
@@ -1,16 +1,10 @@
 package com.davr7.msemail.resources;
 
-import com.davr7.msemail.domain.Email;
-import com.davr7.msemail.dtos.EmailDTO;
 import com.davr7.msemail.dtos.EmailResDTO;
 import com.davr7.msemail.services.EmailService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,13 +14,7 @@ public class EmailResource {
     @Autowired
     EmailService emailServ;
 
-    @PostMapping("/sending-email")
-    public ResponseEntity<Email> handleSendEmail(@RequestBody @Valid EmailDTO emailDto) {
-        var data = emailServ.sendEmail(emailDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(data);
-    }
-
-    @GetMapping("/emails")
+    @GetMapping("/email")
     public ResponseEntity<List<EmailResDTO>> handleReadAllEmails() {
         List<EmailResDTO> list = emailServ.readAllEmails();
         return ResponseEntity.ok().body(list);
